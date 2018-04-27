13c75a5c3cdfda2476631eb33ca9104e55efb06d
Ching-Yi Lin <icequ60914@gmail.com>
2018-04-27 01:03:22 -0700

    added shower test case to cat

--- src/test/java/furryjourney/CatTest.java
+++ src/test/java/furryjourney/CatTest.java
@@ -7,11 +7,18 @@ import static junit.framework.TestCase.assertEquals;
 public class CatTest {
 
     static Cat cat = new Cat();
+    static double MAX_FACTOR = 100.0;
 
     @Test
-    public void dogWalkShouldIncreaseCurrentHappinessByFivePerHour (){
+    public void catWalkShouldIncreaseCurrentHappinessByFivePerHour (){
         double currentHappiness = cat.getHappiness();
         cat.walk(1);
         assertEquals(currentHappiness + 5 * 1, cat.getHappiness());
+    }
+
+    @Test
+    public void catTakeShowerShouldReachMaxCleaniness (){
+        cat.takeShower();
+        assertEquals(MAX_FACTOR, cat.getCleaniness());
     }
 }
